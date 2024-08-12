b13d89b615a5d99eaa0770ced279d2c5bfc49796
Elias Fink <elias.fink@gmx.de>
2024-08-12 15:49:55 +0100

    Analysis script

--- SS_Analysis.py
+++ SS_Analysis.py
@@ -7,4 +7,30 @@ Analyse SAXS data.
 
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
+def transform(arr: np.nadrray) -> np.ndarray:
+    '''
+    Transform detector read in to surface profile
+
+    Args:
+        arr: measured data
+
+    Returns:
+        surface profile
+
+    Raises:
+        ValueError: if input array does not have correct dimensions
+    '''
+    arr = np.sqrt(arr) # convert intensity to amplitude
+
+    if len(np.shape(arr)) == 1:
+        profile = np.fft.fft(arr)
+        plt.plot(range(len(profile)), profile)
+        return profile
+    if len(np.shape(arr)) == 2:
+        profile = np.fft.fft2(arr)
+        plt.imshow(profile)
+        return profile
+    raise ValueError("Input array needs to be one- or two-dimensional.")
