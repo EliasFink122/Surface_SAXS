56ba4945e3860e69d99956c0066833f9f9185385
Elias Fink <elias.fink@gmx.de>
2024-08-14 15:02:33 +0100

    Structuring

--- SS_Analysis.py
+++ SS_Analysis.py
@@ -39,7 +39,7 @@ def transform(arr: np.ndarray, plot = True) -> np.ndarray:
         profile = np.abs(np.fft.ifft(arr))
         profile = np.roll(profile, int(len(profile)/2))
         profile = profile/np.max(profile)
-        # profile = deconvolve(profile)
+        # profile = contrast(profile)
         plt.plot(range(len(profile)), profile)
         plt.xlabel("x [μm]")
         plt.ylabel("Height [μm]")
@@ -54,7 +54,7 @@ def transform(arr: np.ndarray, plot = True) -> np.ndarray:
         profile = np.roll(profile, int(len(profile)/2), 0)
         profile = np.roll(profile, int(len(profile[0])/2), 1)
         profile = profile/np.max(profile)
-        profile = deconvolve(profile)
+        profile = contrast(profile)
         ax1.set_title("1-d surface")
         ax1.plot(range(len(profile[int(len(profile)/2)])),
                  profile[int(len(profile)/2)])
@@ -71,7 +71,7 @@ def transform(arr: np.ndarray, plot = True) -> np.ndarray:
         return profile
     raise ValueError("Input array needs to be one- or two-dimensional.")
 
-def deconvolve(arr: np.ndarray) -> np.ndarray:
+def contrast(arr: np.ndarray) -> np.ndarray:
     '''
     Deconvolve one or two dimensional array
 
@@ -83,14 +83,11 @@ def deconvolve(arr: np.ndarray) -> np.ndarray:
     '''
     if len(np.shape(arr)) == 1:
         for i, val in enumerate(arr):
-            x = i-len(arr)
-            arr[i] = val/np.sinc(x/50)
+            if val > 0.1:
+                arr[i] = 0
         return arr
     for i, row in enumerate(arr):
-        x = i-len(arr)/2
         for j, val in enumerate(row):
-            y = j-len(row)/2
-            #arr[i, j] = val*np.sqrt(np.linalg.norm([x, y]))**2
             if val > 0.1:
                 arr[i, j] = 0
     return arr
